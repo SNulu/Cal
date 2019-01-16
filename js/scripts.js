2cc42000b5d986dcbe1f2ca58096f2d32b519d7f
Sanyu Prado and Slater Smith <sanyu@>
2019-01-16 11:20:28 -0800

    add event handler to connect function to html button and properly read ui

--- js/scripts.js
+++ js/scripts.js
@@ -14,12 +14,12 @@ function divide(number1, number2) {
   return number1 / number2;
 };
 
-
-
 $(document).ready(function() {
-  var number1 = parseInt(prompt("Enter a number:"));
-  var number2 = parseInt(prompt("Enter another number:"));
+  $("form#add").submit(function(event) {
+    event.preventDefault();
+    var number1 = parseInt($("#add1").val());
+    var number2 = parseInt($("#add2").val());
+    alert(add(number1, number2));
+  });
 
-
-  alert(subtract(number1, number2));
 });
