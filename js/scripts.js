6ea5d71b00b4c5f9529c2109dc5f401d3a5cefc6
Sanyu Prado and Slater Smith <sanyu@>
2019-01-16 10:46:41 -0800

    clone and edit html and js to set up form html template and divisions between front end and back end logic

---
+++ js/scripts.js
@@ -0,0 +1,25 @@
+function add(number1, number2) {
+  return number1 + number2;
+};
+
+function subtract(number1, number2) {
+  return number1 - number2;
+};
+
+function multiply(number1, number2) {
+  return number1 * number2;
+};
+
+function divide(number1, number2) {
+  return number1 / number2;
+};
+
+
+
+$(document).ready(function() {
+  var number1 = parseInt(prompt("Enter a number:"));
+  var number2 = parseInt(prompt("Enter another number:"));
+
+
+  alert(subtract(number1, number2));
+});
